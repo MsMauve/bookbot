851951aca5c8ddaa610cff1879a5f43a6075dcd5
MsMauve <zoetropesilver@gmail.com>
2025-02-17 21:35:48 -0700

    refactor and add character_counter

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-    print(word_count(file_contents))
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = word_count(text)
+    character_dict = character_counter(text)
+    print(num_words)
+    print(character_dict)
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
 
 def word_count(book):
     words = book.split()
     return len(words)
 
+def character_counter(book):
+    letter_count = {}
+    uncapitalized_book = book.lower()
+    for s in uncapitalized_book:
+        character_amount = 0
+        if s not in letter_count:
+            letter_count[s] = character_amount
+        letter_count[s] += 1
+    return letter_count
+
 main()
+character_counter()
 
