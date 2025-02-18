ddbdb9e4b797179d9eaa39ed03ffe416b8d1c09c
MsMauve <zoetropesilver@gmail.com>
2025-02-18 00:07:43 -0700

    created character report function

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ def main():
     text = get_book_text(book_path)
     num_words = word_count(text)
     character_dict = character_counter(text)
-    print(num_words)
-    print(character_dict)
+    character_list = character_report(character_dict)
+    report_print(character_list, num_words)
 
 def get_book_text(path):
     with open(path) as f:
@@ -23,6 +23,29 @@ def character_counter(book):
             letter_count[s] = character_amount
         letter_count[s] += 1
     return letter_count
+
+def sort_on(dict):
+    return dict["num"]
+
+def character_report(dict):
+    character_list = []
+    for entry in dict:
+        if entry.isalpha():
+            char_entry = {
+                "char" : entry,
+                "num": dict[entry]
+            }
+            character_list.append(char_entry)
+    character_list.sort(reverse=True, key=sort_on)
+    return character_list
+
+def report_print(character_list, word_count):
+    book_path = "books/frankenstein.txt"
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{word_count} words found in the document")
+    for character in character_list:
+        print(f"The '{character['char']}' was found {character['num']} times")
+    print("--- End report ---")
 
 main()
 
